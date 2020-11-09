1354a5ad127420e9d88a6207453ef26cc006892b
Elio Reyes Silva <resiel@gmail.com>
2020-11-09 16:10:54 -0600

    Use CORS to share resources in API REST.

--- billing-api/src/main/java/com/qosoft/billing/api/controllers/ClientRestController.java
+++ billing-api/src/main/java/com/qosoft/billing/api/controllers/ClientRestController.java
@@ -3,12 +3,14 @@ package com.qosoft.billing.api.controllers;
 import com.qosoft.billing.api.models.entity.Client;
 import com.qosoft.billing.api.models.services.ClientServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class ClientRestController {
