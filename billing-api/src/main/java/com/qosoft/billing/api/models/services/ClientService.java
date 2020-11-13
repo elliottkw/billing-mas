75cda99baa05d4cfc68117632ff5af41f92884d7
Elio Reyes Silva <resiel@gmail.com>
2020-11-13 07:04:14 -0600

    Writing the CRUD methods in the backend ClientService class.

--- billing-api/src/main/java/com/qosoft/billing/api/models/services/ClientService.java
+++ billing-api/src/main/java/com/qosoft/billing/api/models/services/ClientService.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public interface ClientService {
     public List<Client> findAll();
+
+    public Client findById(Long id);
+
+    public Client save(Client client);
+
+    public void delete(Long id);
 }
--- billing-api/src/main/java/com/qosoft/billing/api/models/services/ClientServiceImpl.java
+++ billing-api/src/main/java/com/qosoft/billing/api/models/services/ClientServiceImpl.java
@@ -18,4 +18,22 @@ public class ClientServiceImpl implements ClientService {
     public List<Client> findAll() {
         return (List<Client>) clientDao.findAll();
     }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Client findById(Long id) {
+        return clientDao.findById(id).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public Client save(Client client) {
+        return clientDao.save(client);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        clientDao.deleteById(id);
+    }
 }
